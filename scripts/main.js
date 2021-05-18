81a5b3e809a446b1df94141e95da7dba67dd30fe
Chemmy-jack <kuojack0114@gmail.com>
2021-05-18 23:52:26 +0800

    control nav bar content from main.js to sync in every page

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,14 @@
+const nav_elements = [
+    [ "home", "index.html" ],
+    [ "test 1", "index1.html" ],
+    [ "test 2", "index2.html" ],
+    [ "snake game!", "index3.html" ]
+]
+let nav_str = ""
+for ( var i = 0 ; i < nav_elements.length ; i++ ) {
+    nav_str += "<li><a href='" + nav_elements[i][1] + "'>" + nav_elements[i][0] + "</a></li>\n"
+}
+console.log(nav_str) ;
 $(document).ready(function(){ // or you can type "$(function(){}"
-    $("p").css("font-size", " 30px") ;
-    $("p").click(function(){
-        $(this).hide() ;
-    }) ;
-    $("button").dblclick(function(){
-        $("p").css("font-size", " 50px") ;
-    }) ;
+    $("#nav").html(nav_str) ;
 });

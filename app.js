295130060ed8af9ad2c35a355d7c8f99b87d0814
郭哲明 <jack@Jack-MacBook-Air.local>
2022-01-10 18:21:39 +0800

    now 'node app.js' can get perfectly get index html working, it's a fully functional static web

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http')
+const fs = require('fs')
 const port = 8000
 
 // the function to call every time it recieves request
 const server = http.createServer(function(req, res) {
     console.log('got a request!')
-    res.write('Hello Node')
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('error: something in this server has gone wrong')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 // to set up the server to listen on the port

dabd903075b04dc52fa206571733a50ff3b21635
Chemmy-Jack <kuojack0114@gmail.com>
2022-07-18 17:47:50 +0800

    big update, scout game login function finnished

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const util = require('util');
-const webSocketServer = require('ws').Server;
-const httpport = 80;
-const websocketport = 8000;
+const WebSocket = require('ws');
+const httpport = 8888;
+const websocketport = 8080;
 
-var ws = new webSocketServer({port: websocketport});
+
+
+// websocket client end
 
 function afterDotToFileType(afterDot) {
     switch (afterDot) {
+        case "jpg" :
+            return "image/jpg";
+            break;
         case "ico" :
             return "image/vnd.microsoft.icon";
             break;
@@ -29,21 +34,37 @@ function afterDotToFileType(afterDot) {
 // the function to call every time it recieves request
 const server = http.createServer(function(req, res) {
     console.log('got a request!');
-    let path = url.parse(req.url, true);
-    let file_requested = "." + path.pathname;
-    if (file_requested == "./") { file_requested = "index.html"}
-    let file_afterdot = file_requested.split(".").pop();
-    console.log(path);
-    console.log(afterDotToFileType(file_afterdot));
+    if(req.method == "GET") {
+        console.log("here is a get request");
+        try {
+            let path = url.parse(req.url, true);
+            let file_requested = "." + path.pathname;
+            if (file_requested == "./") { file_requested = "scout_game_JUL26.html"}
+            let file_afterdot = file_requested.split(".").pop();
+            console.log(path);
+            console.log(afterDotToFileType(file_afterdot));
 
-    res.writeHead(200, { 'Content-Type': afterDotToFileType(file_afterdot) });
-    fs.readFile(file_requested, function(error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('error: something in this server has gone wrong');
-        } else { res.write(data); };
-        res.end();
-    });
+            res.writeHead(200, { 'Content-Type': afterDotToFileType(file_afterdot) });
+            fs.readFile(file_requested, function(error, data) {
+                if (error) {
+                    res.writeHead(404);
+                    res.write('error: something in this server has gone wrong');
+                } else { res.write(data); };
+                res.end();
+            });
+        } catch {
+            console.log("not recognized :");
+            console.log(req);
+        };
+    } else if ( req.method == "POST" ) {
+
+        console.log("here is a post request");
+        // let path = url.parse(req.url, true);
+        // console.log(path);
+        console.log(req);
+    } else {
+        console.log("not recongnized method: "+ req.method);
+    }
 });
 
 // to set up the server to listen on the port
@@ -54,3 +75,73 @@ server.listen(httpport, function(error) {
         console.log('Server is listening on port :' + httpport)
     }
 });
+
+// game
+// read database
+let database_raw = fs.readFileSync("scout_game_JUL26.json");
+let database = JSON.parse(database_raw);
+console.log("this is the database")
+console.log(database);
+
+// websocket server end
+const ws_server = new WebSocket.Server({
+    port : websocketport
+});
+console.log("websocket on: " + websocketport);
+
+let sockets = [];
+
+ws_server.on('connection', function(socket) {
+    sockets.push(socket);
+    socket.on('message', function(msg) {
+        let recieved_data;
+        try {
+            recieved_data = JSON.parse(msg);
+            console.log(recieved_data);
+        } catch(error) {
+            console.log("not json");
+            console.log("error: " + error.message);
+            console.log("message: \n" + msg);
+            return 0;
+        };
+
+        if(recieved_data.method == "init"){
+            let success = false;
+            if(recieved_data.team in database.team_list){
+                if(recieved_data.password == database.team_list[recieved_data.team].password){ success = true; };
+                console.log("login successfull");
+            };
+            json_to_send = JSON.stringify( { "method": "init_reply", "successful": success })
+        }
+        socket.send(json_to_send);
+        console.log("sent json back");
+        console.log(json_to_send);
+        // sockets.forEach(s => s.send(json_to_send));
+        // console.log(sockets)
+    });
+    socket.on('close', function() {
+        sockets = sockets.filter(s => s !== socket);
+    });
+});
+
+function nextProblem(team){
+    if (data.team_list[team].remaining != []) {
+        return {
+            "method": "all_done",
+            "team": team,
+        }
+    }else{
+        next_Problem_number=database.team_list[team].remaining.length * Math.random;
+        database.team_list[team].current = database.team_list[team].remaining[next_Problem_number];
+        database.team_list[team].remaining = database.team_list[team].remaining.splice(next_Problem_number, next_Problem_number)
+        let image_path = "images/scout_game_JUL26/" + data.problem_list[database.team_list[team].current].img_path;
+        return {
+            "method": "problem",
+            "discription": data.problem_list[database.team_list[team].current].discription,
+            "img_path": image_path
+        };
+    }
+}
+function answerIsCorrct(recieved_json){
+
+};
---
+++ scripts/scout_game_JUL26.js
@@ -0,0 +1,81 @@
+let server_ip = "localhost";
+let websocket_port = 8080;
+let websocket_url = "ws://" + server_ip + ":" + websocket_port;
+let not_init = true;
+const socket = new WebSocket(websocket_url) ;
+
+socket.onopen = function(event) {
+    console.log("socket succesful connected to" + websocket_url);
+    console.log(event);
+}
+
+socket.onmessage = function(event) {
+    console.log("got ws msg");
+    console.log(event);
+    var recieved_data = JSON.parse(event.data);
+    // only for testing
+    console.log(recieved_data);
+    if(recieved_data.method == "init_reply"){
+        if(recieved_data.successful){
+            $("#problem-body").toggle(0);
+            // $("#login-body").toggle(0);
+            $("#team-input").addClass("disabled readonly").attr("disabled", "true");
+            $("#password-input").addClass("disabled readonly").attr("disabled", "true");
+            $("#login-button").addClass("disabled");
+        }else if(!recieved_data.successful)(
+            alert("login failed!!")
+        );
+    }else if(recieved_data.method == "answer_reply"){
+        if (recived_data.skipped){
+            alert("skipped!!")
+        };
+    };
+}
+
+
+$(document).ready(function(){ // or you can type "$(function(){}"
+    $("#problem-body > div > div").addClass("p-2");
+    $("#problem-body > div").addClass("gx-5");
+    $("#login-session > div > div").addClass("p-2");
+    $("#problem-body > div").addClass("gx-5");
+    $("#problem-body").toggle(0);
+    $("#login-button").click(function(){
+        send_JSON =
+            {
+                "method": "init",
+                "team" : $("#team-input").val(),
+                "password" : $("#password-input").val(),
+            };
+        console.log(send_JSON);
+        socket.send(JSON.stringify(send_JSON));
+    });
+    $("#send-button").click(function(){
+        if (confirm("Are you sure you want to send answer?")){
+            img_src_split = $("#problem-image").attr("src").split("/");
+            problem_number = img_src_split[img_src_split.length - 1].slice(1,-4);
+            send_JSON =
+                {
+                    "method": "answer",
+                    "team" : $("#team-input").val(),
+                    "password" : $("#password-input").val(),
+                    "problem_number" : problem_number,
+                    "answer" : $("#answer-input").val()
+                };
+            console.log(send_JSON);
+            socket.send(JSON.stringify(send_JSON));
+        };
+    });
+    $("#skip-button").click(function(){
+        if(confirm("Are you sure you want to skip?")){
+            send_JSON =
+                {
+                    "method": "skip",
+                    "team" : $("#team-input").val(),
+                    "password" : $("#password-input").val(),
+                    "problem_number" : problem_number,
+                };
+            console.log(send_JSON);
+            socket.send(JSON.stringify(send_JSON));
+        };
+    });
+});
